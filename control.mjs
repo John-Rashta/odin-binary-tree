1f2b85b1b77c954993c686c4e3ee2cac747b2751
John-Rashta <john_rashta@outlook.com>
2024-08-22 22:15:15 +0100

    Finished Project
    Add some extra work on this one, also tried to do my own delete function,
    very diferent from the one in the article, its worse but i wanted
    to try something in my own way

---
+++ control.mjs
@@ -0,0 +1,151 @@
+import treeManagement from "./treeObj.mjs";
+
+function Tree(arr) {
+    if (!Array.isArray(arr)) {
+        return;
+    }
+
+    const getRoot = function getRootOfTree() {
+        return root;
+    }
+
+    const buildTree = function buildATreeFromUnsortedArray(arr) {
+        let newArr = treeManager.sortTheArray(arr);
+
+        return treeManager.makeTheTree(newArr, 0, newArr.length - 1);
+    }
+
+    const insert = function insertValueIntoTree(value) {
+        treeManager.insertValue(root, value);
+    }
+
+    const deleteItem = function deleteValueFromTree(value) {
+        if (!treeManager.findValue(value)) {
+            return;
+        }
+
+        treeManager.deleteValue(root, value);
+    }
+
+    const find = function findValueInTree(value) {
+        return treeManager.findValue(root, value);
+    }
+
+    const levelOrder = function levelOrderIterateThroughTree(funcToCall) {
+        if (typeof funcToCall != "function") {
+            throw new Error("Callback is required");
+        }
+
+        treeManager.levelOrderIteration(root, funcToCall);
+    }
+
+    const preOrder = function preOrderIterateThroughTree(funcToCall) {
+        if (typeof funcToCall != "function") {
+            throw new Error("Callback is required");
+        }
+
+        treeManager.preOrderIteration(root, funcToCall);
+    }
+
+    const inOrder = function inOrderIterateThroughTree(funcToCall) {
+        if (typeof funcToCall != "function") {
+            throw new Error("Callback is required");
+        }
+
+        treeManager.inOrderIteration(root, funcToCall);
+    }
+
+    const postOrder = function postOrderIterateThroughTree(funcToCall) {
+        if (typeof funcToCall != "function") {
+            throw new Error("Callback is required");
+        }
+
+        treeManager.postOrderIteration(root, funcToCall);
+    }
+
+    const height = function getHeightOfNode(node) {
+        if (!treeManager.findValue(node.value)) {
+            return;
+        }
+
+        return treeManager.getHeight(node);
+
+    }
+
+    const depth = function getDepthOfNode(node) {
+        if (!treeManager.findValue(node.value)) {
+            return;
+        }
+
+        return treeManager.getDepth(root, node);
+    }
+
+    const isBalanced = function checkIfTreeIsBalanced() {
+        let leftSide = treeManager.getHeight(root.left);
+        let rightSide = treeManager.getHeight(root.right);
+        let difference = leftSide - rightSide;
+
+        if (difference === -1 || difference === 0 || difference === 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const rebalance = function rebalanceTreeFromScratch() {
+        const tempArray = [];
+        treeManager.levelOrderIteration(root, (node) => tempArray.push(node.value));
+        const sortedArray = treeManager.sortTheArray(tempArray);
+        root = treeManager.makeTheTree(sortedArray, 0 ,sortedArray.length - 1);
+    }
+
+    const treeManager = treeManagement();
+
+    let root = buildTree(arr);
+
+    return {
+        getRoot,
+        buildTree,
+        insert,
+        deleteItem,
+        find,
+        levelOrder,
+        preOrder,
+        inOrder,
+        postOrder,
+        height,
+        depth,
+        isBalanced,
+        rebalance,
+    }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+
+const newTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 87, 11, 44, 36, 98]);
+
+console.log(newTree.isBalanced())
+newTree.insert(55);
+newTree.insert(111);
+newTree.insert(102);
+newTree.insert(142);
+newTree.insert(122);
+console.log(newTree.isBalanced())
+
+newTree.rebalance();
+console.log(newTree.isBalanced())
+newTree.insert(2);
+prettyPrint(newTree.getRoot());
+console.log(newTree.depth(newTree.find(3)))
---
+++ treeObj.mjs
@@ -0,0 +1,204 @@
+import Node from "./nodeObj.mjs";
+
+const makeTheTree = function buildATreeFromSortedArray(arr, start, end) {
+    if (start > end) {
+        return null;
+         
+       }
+        
+        let mid = Math.floor((start + end) / 2);
+        let root = new Node(arr[mid]);
+        root.left = makeTheTree(arr, start, mid-1);
+        root.right = makeTheTree(arr, mid+1, end);
+        return root;
+};
+
+const sortTheArray = function sortArrayToBuildTheTree(arr) {
+    return arr.filter((value, index, itself) => {
+        return index == itself.indexOf(value);
+      }).sort((a, b) => a - b);
+};
+
+const insertValue = function insertValueIntoArray(root, value) {
+    if (root.value === value) {
+        return;
+    }
+
+    if (root.value > value) {
+        if (root.left === null) {
+            root.left = new Node(value);
+            return;
+        }
+
+        insertValue(root.left, value);
+    }else if (root.value < value) {
+        if (root.right === null) {
+            root.right = new Node(value);
+            return;
+        }
+
+        insertValue(root.right, value);
+    }
+};
+
+const findValue = function findValueInTheTree(root, value) {
+    if (root === null || root.value === value) {
+        return root;
+    }
+
+    if (root.value > value) {
+        return findValue(root.left, value);
+    }else if (root.value < value) {
+        return findValue(root.right, value);
+    }
+
+};
+
+const getHeight = function getHeightOfGivenNode(node) {
+    if (node === null) {
+        return -1;
+    }
+
+    let leftHeight = getHeight(node.left);
+    let rightHeight = getHeight(node.right);
+
+    if (leftHeight > rightHeight) {
+        return leftHeight + 1;
+    }
+
+    return rightHeight + 1;
+}
+
+const getDepth = function getDepthOfNode(root, node) {
+    if (root === node) {
+        return 0;
+    }
+
+    if (root.value > node.value) {
+        return getDepth(root.left, node) + 1;
+    }else if (root.value < node.value) {
+        return getDepth(root.right, node) + 1;
+    }
+}
+
+const deleteHelper = function helpDeleteValuesByHavingAllOptions(root) {
+    if (root.left === null) {
+        root.value = root.right.value;
+        root.right = null;
+        return [null, null];
+    }
+
+    if (root.right === null) {
+        root.value = root.left.value;
+        root.left = null;
+        return [null, null];
+    }
+
+    let replacementNode = root.right;
+    while (replacementNode !== null && replacementNode.left !== null) {
+        replacementNode = replacementNode.left;
+    }
+
+    root.value = replacementNode.value;
+    return [root.right, replacementNode.value];
+}
+
+const deleteValue = function deleteValueFromTree(root, value) {
+    if (root === null) {
+        return;
+    }
+
+    if (root.value === value) {
+        const [newRoot, newValue] = deleteHelper(root);
+        if (!newRoot) {
+            return;
+        }
+        if (newRoot.right === null && newRoot.left === null) {
+            root.right = null;
+            return;
+
+        }
+        deleteValue(newRoot, newValue);
+    }else if (root.value > value) {
+        if (root.left.value === value & root.left.right === null & root.left.left === null) {
+            root.left = null;
+            return;
+        }
+        deleteValue(root.left, value);
+    }else if (root.value < value) {
+        if (root.right.value === value & root.right.right === null & root.right.left === null) {
+            root.right = null;
+            return;
+        }
+        deleteValue(root.right, value);
+    }
+}
+
+const levelOrderIteration = function levelOrderIterateOverTree(root, funcToCall) {
+    if (root === null) {
+        return;
+    }
+
+    const arr = [];
+    arr.push(root);
+
+    while(arr.length != 0) {
+        let currentNode = arr[0];
+        funcToCall(currentNode);
+
+        if (currentNode.left != null) {
+            arr.push(currentNode.left); 
+        }
+        if (currentNode.right != null) {
+            arr.push(currentNode.right);
+        }
+        arr.shift();
+    }
+}
+
+const preOrderIteration = function preOrderIterateOverTree(root, funcToCall) {
+    if (root === null) {
+        return;
+    }
+
+    funcToCall(root);
+    preOrderIteration(root.left, funcToCall);
+    preOrderIteration(root.right, funcToCall);
+}
+
+const inOrderIteration = function inOrderIterateOverTree(root, funcToCall) {
+    if (root === null) {
+        return;
+    }
+
+    inOrderIteration(root.left, funcToCall);
+    funcToCall(root);
+    inOrderIteration(root.right, funcToCall);
+}
+
+const postOrderIteration = function postOrderIterateOverTree(root, funcToCall) {
+    if (root === null) {
+        return;
+    }
+
+    postOrderIteration(root.left, funcToCall);
+    postOrderIteration(root.right, funcToCall);
+    funcToCall(root);
+}
+
+
+export default function treeManagement() {
+    return {
+        makeTheTree,
+        sortTheArray,
+        insertValue,
+        findValue,
+        getHeight,
+        getDepth,
+        deleteValue,
+        levelOrderIteration,
+        preOrderIteration,
+        inOrderIteration,
+        postOrderIteration,
+    }
+}
